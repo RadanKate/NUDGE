7b234d4d3c92a9451d6ce19222a29abf4881a83c
Katherina Radan <katherina.radan@alayacare.com>
2020-05-12 15:22:50 -0400

    fix conftest file

--- conftest.py
+++ conftest.py
@@ -3,8 +3,8 @@ from selenium.common.exceptions import WebDriverException
 
 from Pages.Login_page import LoginPage
 from envparse import env
-# from selenium import webdriver
-from appium import webdriver
+from selenium import webdriver as selenium_webdriver
+from appium import webdriver as appium_webdriver
 
 env.read_envfile()
 tenant = env("TENANT")
@@ -13,21 +13,13 @@ password = env("PASSWORD")
 saucelabs_remote = env("REMOTE")
 saucelabs_username = env("SAUCELABS_USER_NAME")
 saucelabs_password = env("SAUCELABS_PASSWORD")
-saucelabs_api_key = "D4567FC495EE4E4CA94E24589D75BC5C"
+saucelabs_api_key = env("API_KEY")
 
 
 # login once before all the tests, using credentials from .env file
 @pytest.fixture
 def login(driver):
     return LoginPage(driver, tenant, username, password)
-
-
-######################################
-import pytest
-import os
-import requests
-import json
-import re
 
 
 def pytest_addoption(parser):
@@ -41,9 +33,15 @@ def data_center(request):
 
 ios_caps = [{
     'platformName': 'iOS',
+    'deviceIds': 'iPhone_XR_free',
+    "platformVersion": "13.3",
     'deviceOrientation': 'portrait',
     'privateDevicesOnly': False,
-    'phoneOnly': True
+    'phoneOnly': True,
+    'acceptSslCerts': True,
+    'appiumVersion': '1.17.0',
+    'additionalWebviewBundleIds': ['process-SafariViewService'],
+    'safariInitialUrl': tenant
 }]
 
 
@@ -60,11 +58,10 @@ def driver(request, data_center):
     else:
         sauce_url = "https://us1.appium.testobject.com/wd/hub"
 
-    driver = webdriver.Remote(sauce_url, desired_capabilities=caps)
+    driver = appium_webdriver.Remote(sauce_url, desired_capabilities=caps)
 
     # This is specifically for SauceLabs plugin.
     # In case test fails after selenium session creation having this here will help track it down.
-    # creates one file per test non ideal but xdist is awful
     if driver:
         print("SauceOnDemandSessionID={} job-name={}\n".format(driver.session_id, test_name))
     else:
@@ -76,17 +73,21 @@ def driver(request, data_center):
 
 
 ######################################
+
+# testing with a simulator
+
 # @pytest.fixture
 # def driver():
 #     if saucelabs_remote == "True":
 #         desired_cap = {
-#         'platformName':     'iOS',
-#         'deviceOrientation':'portrait',
-#         'privateDevicesOnly': False,
-#         'phoneOnly': True
-# }
+#             "deviceName": "iPhone X Simulator",
+#             "browserName": "Safari",
+#             "deviceOrientation": "portrait",
+#             "platformVersion": "12.2",
+#             "platformName": "iOS"
+#         }
 #         SAUCELABS_URL = 'https://' + saucelabs_username + ':' + saucelabs_password + '@ondemand.saucelabs.com:443/wd/hub'
-#         driver = webdriver.Remote(command_executor=SAUCELABS_URL, desired_capabilities=desired_cap)
+#         driver = selenium_webdriver.Remote(command_executor=SAUCELABS_URL, desired_capabilities=desired_cap)
 #     else:
 #         desired_cap = {
 #             "browserName": "Chrome",
@@ -101,9 +102,11 @@ def driver(request, data_center):
 #                     "userAgent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_4_1 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Chrome/80.0.3987.149 Mobile/14E5239e Safari/602.1"}
 #             }
 #         }
-#         driver = webdriver.Chrome(desired_capabilities=desired_cap)
+#         driver = selenium_webdriver.Chrome(desired_capabilities=desired_cap)
 #     yield driver
 #     driver.quit()
+
+######################################
 
 
 @pytest.fixture
